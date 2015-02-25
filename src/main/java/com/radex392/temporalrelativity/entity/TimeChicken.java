e1a26b7855872f0a3a9353418d6263eb2d8c9910
logan <logan.deheer@gmail.com>
2015-02-25 22:17:03 +1300

    adding ai to the time chicken

--- src/main/java/com/radex392/temporalrelativity/entity/TimeChicken.java
+++ src/main/java/com/radex392/temporalrelativity/entity/TimeChicken.java
@@ -1,15 +1,35 @@
 package com.radex392.temporalrelativity.entity;
 
+import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.ai.*;
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.world.World;
 
 /**
  * Created by OEM on 25/02/2015.
  */
-public class TimeChicken extends EntityMob{
+public class TimeChicken extends EntityAnimal{
 
     public TimeChicken( World world ) {
         super( world );
+
+        this.setSize(0.3F, 0.7F);
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(1, new EntityAIPanic(this, 1.4D));
+        this.tasks.addTask(2, new EntityAIMate(this, 1.0D));
+        this.tasks.addTask(3, new EntityAITempt(this, 1.0D, Items.wheat_seeds, false));
+        this.tasks.addTask(4, new EntityAIFollowParent(this, 1.1D));
+        this.tasks.addTask(5, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+        this.tasks.addTask(7, new EntityAILookIdle(this));
+    }
+
+    @Override
+    public EntityAgeable createChild( EntityAgeable entity ) {
+        return null;
     }
 
 }
